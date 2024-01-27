4ca75c92fd903f69fe7f91e5a0566604d0cc4bb2
Phoenix <127796122+Phoenix-999@users.noreply.github.com>
2024-01-27 10:04:12 +0000

    Update Installing-Grafana-Monitoring.sh

--- Installing-Grafana-Monitoring.sh
+++ Installing-Grafana-Monitoring.sh
@@ -70,9 +70,33 @@ sudo apt install -y sudo wget
 # Install Prometheus
 ###############################
 
-wget https://github.com/prometheus/prometheus/releases/download/v2.49.1/prometheus-2.49.1.linux-amd64.tar.gz
-tar xzf prometheus-2.49.1.linux-amd64.tar.gz
-sudo mv prometheus-2.49.1.linux-amd64 /etc/prometheus
+# Get processor architecture
+ARCH=$(uname -m)
+
+# Set Prometheus version
+PROMETHEUS_VERSION_AMD64="2.49.1"
+PROMETHEUS_VERSION_ARM64="2.45.3"
+
+# Set Prometheus file names
+PROMETHEUS_FILE_AMD64="prometheus-${PROMETHEUS_VERSION_AMD64}.linux-amd64.tar.gz"
+PROMETHEUS_FILE_ARM64="prometheus-${PROMETHEUS_VERSION_ARM64}.linux-arm64.tar.gz"
+
+# Download and extract Prometheus based on architecture
+if [ "$ARCH" == "x86_64" ]; then
+    wget "https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION_AMD64}/${PROMETHEUS_FILE_AMD64}"
+    tar xzf "$PROMETHEUS_FILE_AMD64"
+    PROMETHEUS_DIR="prometheus-${PROMETHEUS_VERSION_AMD64}.linux-amd64"
+elif [ "$ARCH" == "aarch64" ]; then
+    wget "https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION_ARM64}/${PROMETHEUS_FILE_ARM64}"
+    tar xzf "$PROMETHEUS_FILE_ARM64"
+    PROMETHEUS_DIR="prometheus-${PROMETHEUS_VERSION_ARM64}.linux-arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+# Move Prometheus directory
+sudo mv "$PROMETHEUS_DIR" /etc/prometheus
 
 # Create Prometheus systemd service file
 cat <<EOL | sudo tee /etc/systemd/system/prometheus.service
@@ -95,17 +119,40 @@ sudo systemctl restart prometheus
 sudo systemctl enable prometheus
 sudo systemctl status prometheus | grep "Active: active" | sed -e "s/Active: active/$(echo -e "${GREEN}Active: active${NC}")/" && print_success "$(echo -e "\e[1;33m[✓] Prometheus installed successfully\e[0m")"
 
-
 # Allow port 9090
 sudo ufw allow 9090/tcp
+
 
 ###############################
 # Download Node Exporter
 ###############################
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
-tar xzf node_exporter-1.7.0.linux-amd64.tar.gz
-sudo mv node_exporter-1.7.0.linux-amd64 /etc/node_exporter
+# Get processor architecture
+ARCH=$(uname -m)
+
+# Set Node Exporter version
+NODE_EXPORTER_VERSION="1.7.0"
+
+# Set Node Exporter file names
+NODE_EXPORTER_FILE_AMD64="node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
+NODE_EXPORTER_FILE_ARM64="node_exporter-${NODE_EXPORTER_VERSION}.linux-arm64.tar.gz"
+
+# Download and extract Node Exporter based on architecture
+if [ "$ARCH" == "x86_64" ]; then
+    wget "https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/${NODE_EXPORTER_FILE_AMD64}"
+    tar xzf "$NODE_EXPORTER_FILE_AMD64"
+    NODE_EXPORTER_DIR="node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64"
+elif [ "$ARCH" == "aarch64" ]; then
+    wget "https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/${NODE_EXPORTER_FILE_ARM64}"
+    tar xzf "$NODE_EXPORTER_FILE_ARM64"
+    NODE_EXPORTER_DIR="node_exporter-${NODE_EXPORTER_VERSION}.linux-arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+# Move Node Exporter directory
+sudo mv "$NODE_EXPORTER_DIR" /etc/node_exporter
 
 # Create Node Exporter systemd service file
 cat <<EOL | sudo tee /etc/systemd/system/node_exporter.service
@@ -128,7 +175,6 @@ sudo systemctl restart node_exporter
 sudo systemctl enable node_exporter
 sudo systemctl status node_exporter | grep "Active: active" | sed -e "s/Active: active/$(echo -e "${GREEN}Active: active${NC}")/" && print_success "$(echo -e "\e[1;33m[✓] Node Exporter installed successfully\e[0m")"
 
-
 # Remove Existing Prometheus Configuration
 sudo rm -f /etc/prometheus/prometheus.yml
 
@@ -149,25 +195,60 @@ sudo systemctl restart prometheus
 sudo systemctl enable prometheus
 sudo systemctl status prometheus | grep "Active: active" | sed -e "s/Active: active/$(echo -e "${GREEN}Active: active${NC}")/" && print_success "$(echo -e "\e[1;33m[✓] Prometheus configured successfully\e[0m")"
 
-
 ###############################
 # Install Grafana
 ###############################
 
+# Get processor architecture
+ARCH=$(uname -m)
+
+# Set Grafana version
+GRAFANA_VERSION_AMD64="10.2.3"
+GRAFANA_VERSION_ARM64="10.3.1"
+
+# Install dependencies
 sudo apt-get install -y adduser libfontconfig1 musl
-wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.2.3_amd64.deb
-sudo dpkg -i grafana-enterprise_10.2.3_amd64.deb
+
+# Set Grafana file names
+GRAFANA_FILE_AMD64="grafana-enterprise_${GRAFANA_VERSION_AMD64}_amd64.deb"
+GRAFANA_FILE_ARM64="grafana-enterprise_${GRAFANA_VERSION_ARM64}_arm64.deb"
+
+# Function to wait for dpkg lock to be released
+wait_for_dpkg_lock() {
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1 ; do
+        echo "Waiting for dpkg lock to be released..."
+        sleep 1
+    done
+}
+
+# Wait for dpkg lock
+wait_for_dpkg_lock
+
+# Download and install Grafana based on architecture
+if [ "$ARCH" == "x86_64" ]; then
+    wget "https://dl.grafana.com/enterprise/release/${GRAFANA_FILE_AMD64}"
+    sudo dpkg -i "$GRAFANA_FILE_AMD64"
+elif [ "$ARCH" == "aarch64" ]; then
+    wget "https://dl.grafana.com/enterprise/release/${GRAFANA_FILE_ARM64}"
+    sudo dpkg -i "$GRAFANA_FILE_ARM64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
 
 # Restart, enable, and check status of Grafana
 sudo systemctl restart grafana-server
 sudo systemctl enable grafana-server
 sudo systemctl status grafana-server | grep "Active: active" | sed -e "s/Active: active/$(echo -e "${GREEN}Active: active${NC}")/" && print_success "$(echo -e "\e[1;33m[✓] Grafana installed successfully\e[0m")"
 
-
 # Allow port 3000
 sudo ufw allow 3000/tcp
 
-# Cleanup unnecessary files
+###############################
+# Optimizing Storage
+###############################
+
+# Deleting & Cleanup Unnecessary Files
 cleanup && print_success "$(echo -e "\e[1;33m[✓] Cleanup completed successfully\e[0m")"
 
 
@@ -181,7 +262,7 @@ echo -e "${GREEN}Prometheus & Grafana Monitoring installed and configured succes
 echo -e "${PURPLE}Please configure the Grafana Dashboard by navigating to the web address below${NC}"
 echo -e "${RED}http://$server_ip:3000/${NC}"
 echo -e "${PURPLE}Please following the visual instructions on the GitHub page${NC}"
-echo -e "${CYAN}https://github.com/Phoenix-999/Installing-Grafana-Monitoring${NC}"
+echo -e "${CYAN}https://github.com/Phoenix-999${NC}"
 echo -e "${BLUE}***********************************************************************************${NC}"
 
 ###############################
